12f4755c4d7923b81e0871064d6b628a7406792c
Baptiste Jonglez <baptiste--git@jonglez.org>
2013-12-16 12:13:41 +0100

    Use datetime objects to store dates, instead of our own format
    This allows us to check availability of a room for timespans of more than a day.

--- scripts/check_dispo_monod.py
+++ scripts/check_dispo_monod.py
@@ -4,17 +4,10 @@ import datetime
 from datetime import timedelta
 
 
-class Time(datetime.datetime):
-    """Extends the datetime.datetime class to look like datetime.time, but
-    with support for substraction and all the nice stuff."""
-    def __new__(cls, hour=0, minute=0, second=0, microsecond=0, tzinfo=None):
-        return super(Time, cls).__new__(cls, 2000, 1, 1, hour, minute, second, microsecond, tzinfo)
-
-
-def parse_monod(year, month, day, area, room):
+def parse_monod(area, room, date):
     """Returns the list of hours at which the given room is occupied."""
     skeleton = 'https://glaive-03-domu-lamp.ens-lyon.fr/ENT/mrbs-enseignement/day.php?'
-    args = 'year=%i&month=%i&day=%i&area=%i&room=%i&debug_flag=1' % (year, month, day, area, room)
+    args = 'year=%i&month=%i&day=%i&area=%i&room=%i&debug_flag=1' % (date.year, date.month, date.day, area, room)
 
     f = urllib.urlopen(skeleton + args)
     raw = f.readlines()
@@ -28,15 +21,15 @@ def parse_monod(year, month, day, area, room):
                 data.append(groups)
     # Each item in data is a triple (room, day, hour). We only need the hour.
     hours = [d[2] for d in data if int(d[0]) == room]
-    # The format is weird: "HHMM". Convert it to a (hours, minutes) tuple.
-    return [Time(int(h[0:-2]), int(h[-2:len(h)])) for h in hours]
+    # The format is weird: "HHMM". Convert it to a datetime object.
+    return [datetime.datetime.combine(date.date(), datetime.time(int(h[0:-2]), int(h[-2:len(h)]))) for h in hours]
 
 
-def occupancy_intervals(year, month, day, area, room):
+def occupancy_intervals(area, room, date):
     """Returns the list of intervals at which the room is occupied"""
     # Length of a single time slot.
     TIMESLOT = timedelta(minutes=30)
-    raw_data = parse_monod(year, month, day, area, room)
+    raw_data = parse_monod(area, room, date)
     if not raw_data:
         return []
     intervals = list()
@@ -49,11 +42,15 @@ def occupancy_intervals(year, month, day, area, room):
     return intervals
 
 
-def is_room_free(year, month, day, area, room,
-                 start_hour, start_min,
-                 stop_hour, stop_min):
-    intervals = occupancy_intervals(year, month, day, area, room)
-    start = Time(start_hour, start_min)
-    stop = Time(stop_hour, stop_min)
-    clashing = filter(lambda (t1, t2): start < t2 and t1 < stop, intervals)
+def is_room_free(area, room, start_time, stop_time):
+    """Checks if a room in Monod is free for the specified time
+    interval. Arguments are datetime objects. The time interval is allowed
+    to span multiple days."""
+    ONEDAY = datetime.timedelta(1)
+    intervals = list()
+    day = start_time
+    while day < stop_time:
+        intervals.extend(occupancy_intervals(area, room, day))
+        day += ONEDAY
+    clashing = filter(lambda (t1, t2): start_time < t2 and t1 < stop_time, intervals)
     return (not clashing)
